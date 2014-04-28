d65bb29467ef05208b31461c91d1488236b90ef3
Yao Xiao <xiao67@data.cs.purdue.edu>
2014-04-28 01:56:27 -0400

    new apache

---
+++ apache/include/ap_config_layout.h
@@ -0,0 +1,64 @@
+/* Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file  ap_config_layout.h
+ * @brief Apache Config Layout
+ */
+
+#ifndef AP_CONFIG_LAYOUT_H
+#define AP_CONFIG_LAYOUT_H
+
+/* Configured Apache directory layout */
+#define DEFAULT_PREFIX "/homes/xiao67/apache"
+#define DEFAULT_EXP_EXEC_PREFIX "/homes/xiao67/apache"
+#define DEFAULT_REL_EXEC_PREFIX ""
+#define DEFAULT_EXP_BINDIR "/homes/xiao67/apache/bin"
+#define DEFAULT_REL_BINDIR "bin"
+#define DEFAULT_EXP_SBINDIR "/homes/xiao67/apache/bin"
+#define DEFAULT_REL_SBINDIR "bin"
+#define DEFAULT_EXP_LIBEXECDIR "/homes/xiao67/apache/modules"
+#define DEFAULT_REL_LIBEXECDIR "modules"
+#define DEFAULT_EXP_MANDIR "/homes/xiao67/apache/man"
+#define DEFAULT_REL_MANDIR "man"
+#define DEFAULT_EXP_SYSCONFDIR "/homes/xiao67/apache/conf"
+#define DEFAULT_REL_SYSCONFDIR "conf"
+#define DEFAULT_EXP_DATADIR "/homes/xiao67/apache"
+#define DEFAULT_REL_DATADIR ""
+#define DEFAULT_EXP_INSTALLBUILDDIR "/homes/xiao67/apache/build"
+#define DEFAULT_REL_INSTALLBUILDDIR "build"
+#define DEFAULT_EXP_ERRORDIR "/homes/xiao67/apache/error"
+#define DEFAULT_REL_ERRORDIR "error"
+#define DEFAULT_EXP_ICONSDIR "/homes/xiao67/apache/icons"
+#define DEFAULT_REL_ICONSDIR "icons"
+#define DEFAULT_EXP_HTDOCSDIR "/homes/xiao67/apache/htdocs"
+#define DEFAULT_REL_HTDOCSDIR "htdocs"
+#define DEFAULT_EXP_MANUALDIR "/homes/xiao67/apache/manual"
+#define DEFAULT_REL_MANUALDIR "manual"
+#define DEFAULT_EXP_CGIDIR "/homes/xiao67/apache/cgi-bin"
+#define DEFAULT_REL_CGIDIR "cgi-bin"
+#define DEFAULT_EXP_INCLUDEDIR "/homes/xiao67/apache/include"
+#define DEFAULT_REL_INCLUDEDIR "include"
+#define DEFAULT_EXP_LOCALSTATEDIR "/homes/xiao67/apache"
+#define DEFAULT_REL_LOCALSTATEDIR ""
+#define DEFAULT_EXP_RUNTIMEDIR "/homes/xiao67/apache/logs"
+#define DEFAULT_REL_RUNTIMEDIR "logs"
+#define DEFAULT_EXP_LOGFILEDIR "/homes/xiao67/apache/logs"
+#define DEFAULT_REL_LOGFILEDIR "logs"
+#define DEFAULT_EXP_PROXYCACHEDIR "/homes/xiao67/apache/proxy"
+#define DEFAULT_REL_PROXYCACHEDIR "proxy"
+
+#endif /* AP_CONFIG_LAYOUT_H */
